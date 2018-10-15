ebb9b15c960eea68650db8d1f8518effc5a0f245
Smit27 <smith29raval@gmail.com>
2018-10-15 14:53:08 +0530

    Added header footer with bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
+import $ from 'jquery';
 
+/************* Custom *************/
+import Header from './common/header.js';
+import Footer from './common/footer.js';
+
+/************* Bootstrap *************/
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/************* Stylesheets *************/
 import "../../public/css/App.css";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-			<h1>Hello, World!!</h1>
+				<Header />
+				<div className="container">
+					<h1>Hello World!!</h1>
+				</div>
+				<Footer />
 			</div>
 			);
 	}
